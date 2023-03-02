2b3fed885ce15fdf970f0e3bce48d92fc6678660
Pixel <pixelhir@outlook.com>
2023-03-02 19:24:43 +0100

    Add project files.

---
+++ UEWPF4_2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UEWPF4_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Person> ListOfPersons = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitBinding();
+        }
+        private void InitBinding()
+        {
+            ListOfPersons = new List<Person>();
+            ListOfPersons.Add(new Person(1, "Adam", "Kowalski", 24));
+            ListOfPersons.Add(new Person(2, "Jan", "Kowalski", 23));
+            ListOfPersons.Add(new Person(3, "Agnieszka", "Kowalczyk", 28));
+            ListOfPersons.Add(new Person(4, "Janusz", "Abacki", 25));
+            lstPersons.ItemsSource = ListOfPersons;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lstPersons.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("LastName", ListSortDirection.Ascending));
+        }
+
+        private void RefreshList()
+        {
+            CollectionViewSource.GetDefaultView(lstPersons.ItemsSource).Refresh();
+        }
+        private void lstPersons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var np = new Person(0, "Imię", "Nazwisko", 0);
+            ListOfPersons.Add(np);
+            RefreshList();
+            // Automatycznie focusujemy nowo dodany element
+            lstPersons.SelectedItem = np;
+        }
+
+        private void delBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var item = lstPersons.SelectedItem as Person;
+            //null safety
+            if(item == null) return;
+            ListOfPersons.Remove(item);
+            RefreshList();
+        }
+    }
+}
---
+++ UEWPF4_2/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+namespace UEWPF4_2
+{
+    class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person(int id, string fn, string ln, int ag)
+        {
+            PersonId = id;
+            FirstName = fn;
+            LastName = ln;
+            Age = ag;
+        }
+    public override string ToString()
+    {
+        return $"{PersonId} {FirstName} {LastName} {Age}";
+    }
+}
+}
